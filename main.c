749ab699d3d397341899a40ccaad5b0e931a39f4
Tim Shen <innocentsty@gmail.com>
2012-03-27 16:13:47 +0800

    ...

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "malloc.h"
 
 #define MAX 100000
 void * a[MAX];
@@ -9,20 +8,32 @@ void * a[MAX];
 int main() {
     srandom(0);
     int i = 0;
-    while (1) {
-        if (i == 0 || (i < MAX && random() > RAND_MAX / 2)) {
-            int size = 16384;
+    int times = 1000000;
+    while (times--) {
+        long cho = random();
+        if (i == 0 || (i < MAX && cho >= RAND_MAX / 3 * 2)) {
+            int size = random() % 16384;
+            //fprintf(stderr, "malloc(%u)\n", size);
             if ((a[i] = malloc(size)) != NULL) {
                 i++;
             }
-            //fprintf(stderr, "malloc(%llu) = %p\n", size, a[i-1]);
-        } else {
+        } else if (cho < RAND_MAX / 3){
             int now = random() % i;
             //fprintf(stderr, "free(%p)\n", a[now]);
             free(a[now]);
             a[now] = a[--i];
+        } else {
+            int now = random() % i;
+            int size = random() % 16384;
+            //fprintf(stderr, "realloc(%p, %u)\n", a[now], size);
+            if ((a[now] = realloc(a[now], size)) == NULL) {
+                a[now] = a[--i];
+            }
         }
-        //printf("\n");
+        //fprintf(stderr, "\n");
+    }
+    while (i) {
+        free(a[--i]);
     }
     return 0;
 };
--- malloc.c
+++ malloc.c
@@ -4,7 +4,8 @@
 #include <unistd.h>
 
 #define POOL_SIZE (1<<30) // bytes
-#define NEXT(chunk) ((void *)chunk + chunk->size)
+#define NEXT(chunk) ((void *)(chunk) + (chunk)->size)
+#define min(a, b) ((a) < (b) ? (a) : (b))
 
 struct my_chunk {
     size_t size;
@@ -12,23 +13,22 @@ struct my_chunk {
     struct my_chunk * next;
 };
 
-struct my_chunk_rsv {
-    size_t size;
-};
-
-static void my_init_hook(void);
+static void my_init(void);
 static void * my_malloc(size_t, const void *);
 static void my_free(void *, const void *);
 static void * my_realloc(void *, size_t, const void *);
-static void * _alloc(struct my_chunk *, size_t);
-static void * _locate_rsv_chunk(void *);
+static void _alloc(struct my_chunk *, size_t);
+static void * _malloc_algo(size_t);
+static struct my_chunk * _locate_rsv_chunk(void *);
 static struct my_chunk * _try_merge(struct my_chunk *);
+static void _insert_behind(struct my_chunk *, struct my_chunk *);
+static void _delete(struct my_chunk *);
 
-void (*__malloc_initialize_hook)(void) = my_init_hook;
+void (*__malloc_initialize_hook)(void) = my_init;
 static void * mem;
-static struct my_chunk aval[1];
+static struct my_chunk avail[1];
 
-static void my_init_hook(void) {
+static void my_init(void) {
     if (POOL_SIZE < sizeof(struct my_chunk)) {
         exit(1);
     }
@@ -36,35 +36,30 @@ static void my_init_hook(void) {
     __malloc_hook = my_malloc;
     __free_hook = my_free;
     __realloc_hook = my_realloc;
-    aval->next = mem;
-    aval->prev = mem;
-    ((struct my_chunk *)mem)->prev = aval;
-    ((struct my_chunk *)mem)->next = aval;
+    avail->next = mem;
+    avail->prev = mem;
+    ((struct my_chunk *)mem)->prev = avail;
+    ((struct my_chunk *)mem)->next = avail;
     ((struct my_chunk *)mem)->size = POOL_SIZE;
 };
 
 static void * my_malloc(size_t size, const void * caller) {
-    size += sizeof(struct my_chunk_rsv);
+    size += sizeof(struct my_chunk);
     struct my_chunk * p;
-    for (p = aval->next; p != aval; p = p->next) {
-        if (p->size >= size) {
-            return _alloc(p, size);
-        }
+    if ((p = _malloc_algo(size)) == NULL) {
+        return NULL;
     }
-    return NULL;
+    _alloc((void *)p, size);
+    return p + 1;
 };
 
 static void my_free(void * ptr, const void * caller) {
-    ptr -= sizeof(struct my_chunk_rsv);
+    ptr -= sizeof(struct my_chunk);
     struct my_chunk * left;
     if ((left = _locate_rsv_chunk(ptr)) == NULL) {
         exit(1);
     }
-    struct my_chunk * p = ptr;
-    p->prev = left;
-    p->next = left->next;
-    p->prev->next = p;
-    p->next->prev = p;
+    _insert_behind(left, ptr);
     _try_merge(_try_merge(left));
     return;
 };
@@ -75,33 +70,53 @@ static void * my_realloc(void * ptr, size_t size, const void * caller) {
     }
     if (size == 0) {
         my_free(ptr, caller);
+        return NULL;
     }
-    size += sizeof(struct my_chunk_rsv);
+    ptr -= sizeof(struct my_chunk);
+    size += sizeof(struct my_chunk);
     struct my_chunk * left;
     if ((left = _locate_rsv_chunk(ptr)) == NULL) {
         exit(1);
     }
-    struct my_chunk_rsv * p = ptr;
-    { // TODO in-place realloc
-    }
-    void * ret = malloc(size - sizeof(struct my_chunk_rsv)); 
-    if (ret != NULL) {
-        memcpy(ret, ptr, size - sizeof(struct my_chunk_rsv));
-        free(ptr);
-    }
-    return ret;
-};
 
-static void * _locate_rsv_chunk(void * ptr) {
-    if (ptr < mem || ptr > mem + POOL_SIZE - sizeof(struct my_chunk_rsv)) {
+    struct my_chunk * p = ptr;
+    if (size <= p->size || (NEXT(p) == left->next && size <= p->size + left->next->size)) {
+        _insert_behind(left, p);
+        _try_merge(p);
+        _alloc(p, size);
+        return p + 1;
+    }
+    struct my_chunk * ret;
+    _insert_behind(left, p);
+    _try_merge(_try_merge(left));
+    if ((ret = _malloc_algo(size)) == NULL) {
         return NULL;
     }
-    struct my_chunk * left = aval;
-    while (left->next != aval && (void *)left->next <= ptr) {
+    memmove(ret + 1, p + 1, p->size - sizeof(struct my_chunk));
+    _alloc(ret, size);
+    return ret + 1;
+};
+
+static void * _malloc_algo(size_t size) {
+    struct my_chunk * p;
+    for (p = avail->next; p != avail; p = p->next) {
+        if (p->size >= size) {
+            return p;
+        }
+    }
+    return NULL;
+};
+
+static struct my_chunk * _locate_rsv_chunk(void * ptr) {
+    if (ptr < mem || ptr > mem + POOL_SIZE - sizeof(struct my_chunk)) {
+        return NULL;
+    }
+    struct my_chunk * left = avail;
+    while (left->next != avail && (void *)left->next <= ptr) {
         left = left->next;
     }
-    struct my_chunk_rsv * p;
-    if (left == aval) {
+    struct my_chunk * p;
+    if (left == avail) {
         p = mem;
     } else {
         p = NEXT(left);
@@ -115,30 +130,37 @@ static void * _locate_rsv_chunk(void * ptr) {
     return left;
 };
 
-static void * _alloc(struct my_chunk * ptr, size_t size) {
+static void _alloc(struct my_chunk * ptr, size_t size) {
     size_t size_left = ptr->size - size;
-    if (size_left < sizeof(struct my_chunk_rsv)) {
-        ptr->prev->next = ptr->next;
-        ptr->next->prev = ptr->prev;
-        return (struct my_chunk_rsv *)ptr + 1;
+    if (size_left < sizeof(struct my_chunk)) {
+        _delete(ptr);
+    } else {
+        ptr->size = size;
+        struct my_chunk * new_chunk = NEXT(ptr);
+        new_chunk->size = size_left;
+        _insert_behind(ptr, new_chunk);
+        _delete(ptr);
     }
-    ptr->size = size;
-    struct my_chunk * new_chunk = NEXT(ptr);
-    new_chunk->size = size_left;
-    new_chunk->prev = ptr->prev;
-    new_chunk->next = ptr->next;
-    new_chunk->prev->next = new_chunk;
-    new_chunk->next->prev = new_chunk;
-    return (struct my_chunk_rsv *)ptr + 1;
+    return;
 };
 
 static struct my_chunk * _try_merge(struct my_chunk * left) {
-    if (left != aval && left->next != aval && NEXT(left) == left->next) {
+    if (left != avail && left->next != avail && NEXT(left) == left->next) {
         left->size += left->next->size;
-        struct my_chunk * t = left->next;
-        t->prev->next = t->next;
-        t->next->prev = t->prev;
+        _delete(left->next);
         return left;
     }
     return left->next;
+};
+
+static void _insert_behind(struct my_chunk * left, struct my_chunk * ptr) {
+    ptr->prev = left;
+    ptr->next = left->next;
+    ptr->prev->next = ptr;
+    ptr->next->prev = ptr;
+};
+
+static void _delete(struct my_chunk * ptr) {
+    ptr->prev->next = ptr->next;
+    ptr->next->prev = ptr->prev;
 };
